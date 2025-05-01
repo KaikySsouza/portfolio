b02b0bdaa28937109e66fa935ca6f8c5b1c84a21
Kaiky Souza <kaikybelga@gmail.com>
2025-05-01 01:23:11 -0300

    Add SVG icons for MongoDB, Node.js, React, and Tailwind CSS
    - Added mongodb-plain-wordmark.svg to the icons directory.
    - Added nodejs-original.svg to the icons directory.
    - Added react-original.svg to the icons directory.
    - Added tailwindcss-original.svg to the icons directory.

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,29 +1,70 @@
 function About() {
   return (
     <div>
-      <section>
-        <div className="flex items-center min-h-screen justify-center  ">
-            <img width={510} src="/images/solo.img2.jpg" alt="solo.png" className="rounded-2xl" />
-          <h2 className="text-[25px] bg-[#1F2937] w-[70vh] p-[40px] m-20 rounded-2xl text-gray-100 ">
-            Sou um desenvolvedor web focado em criar interfaces modernas,
-            dinâmicas e intuitivas. Tenho sólida experiência com React.js,
-            JavaScript, HTML e CSS, o que me permite desenvolver aplicações
-            responsivas, acessíveis e com ótima usabilidade. No back-end,
-            utilizo Node.js para criar APIs eficientes e escaláveis. Estou em
-            constante aprendizado, sempre buscando evoluir como desenvolvedor
-            full stack e aplicar soluções criativas aos desafios do dia a dia.
-            Fora do ambiente de desenvolvimento, gosto de jogar basquete,
-            treinar na academia e aproveitar momentos de lazer, atividades que
-            fortalecem meu foco, disciplina e equilíbrio — qualidades que também
-            aplico no meu trabalho.
-          </h2>
-          
-          <div>
+      <section className="flex items-center justify-center min-h-screen">
+        <div className="flex">
+          <img
+            width={510}
+            src="/images/solo.img2.jpg"
+            alt="solo.png"
+            className="rounded-2xl m-20"
+          />
+          <div className="bg-[#1F2937] p-8 rounded-2xl  mx-auto break-words ">
+            <h2 className="text-[25px] w-[60vh] font-light m-6 text-justify text-gray-100 leading-relaxed ">
+              Sou um desenvolvedor web focado em criar interfaces modernas,
+              dinâmicas e intuitivas. Tenho sólida experiência com React.js,
+              JavaScript, HTML e CSS, o que me permite desenvolver aplicações
+              responsivas, acessíveis e com ótima usabilidade. No back-end,
+              utilizo Node.js para criar APIs eficientes e escaláveis. Estou em
+              constante aprendizado, sempre buscando evoluir como desenvolvedor
+              full stack e aplicar soluções criativas aos desafios do dia a dia.
+              Fora do ambiente de desenvolvimento, gosto de jogar basquete,
+              treinar na academia e aproveitar momentos de lazer, atividades que
+              fortalecem meu foco, disciplina e equilíbrio — qualidades que
+              também aplico no meu trabalho.
+            </h2>
           </div>
+        </div>
+      </section>
 
-          
-        
+      <section className="flex ">
+        <div className="block">
+          <h2 className="text-center text-[45px] text-white">Habilidades e Serviços</h2>
+          <p className="text-center text-[25px] text-white">
+            Estas são minhas habilidades de trabalho e serviços que realizei.
+          </p>
+          <div className="flex text-center items-center justify-center  mt-3 ">
 
+            <div className="m-20 bg-[#1a1b26] p-[30px] rounded-2xl">
+              <img src="/icons/react-original.svg" alt="react.svg" width={110} className="ml-[33%] " />
+              <span className="text-[25px] text-white">React</span>
+              <p className="text-[15px] text-white">
+                React.js para desenvolvimento de front-end e interface de usuário.
+              </p>
+            </div>
+
+            <div className="m-20 bg-[#1a1b26] p-[30px] rounded-2xl">
+              <img src="/icons/nodejs-original.svg" alt="node.svg" width={110} className="ml-[33%] " />
+              <span className="text-[25px] text-white">Node.JS e Express.js</span>
+              <p className="text-[15px] text-white">
+                Node.JS e Express.js para desenvolvimento de APIs de backend.
+              </p>
+            </div>
+
+            <div className="m-20 bg-[#1a1b26] p-[30px] rounded-2xl">
+              <img src="/icons/mongodb-plain-wordmark.svg" alt="mongodb.svg" width={110} className="ml-[33%] " />
+              <span className="text-[25px] text-white">Banco de dados MongoDB</span>
+              <p className="text-[15px] text-white">
+                MongoDB para gerenciamento de banco de dados da API de backend.
+              </p>
+            </div>
+
+            <div className="m-20 bg-[#1a1b26] p-[30px] rounded-2xl">
+              <img src="/icons/tailwindcss-original.svg" alt="tailwind.svg" width={110} className="ml-[33%]" />
+              <span className="text-[25px] text-white">TailwindCSS e ShadcnUI</span>
+              <p className="text-[15px] text-white">TailwindCSS é um framework CSS que prioriza a utilidade.</p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -7,39 +7,45 @@ function Home() {
       <section className="flex items-center justify-center min-h-screen ">
         <div className=" flex ">
           <div className="bg-[#1F2937] p-8 rounded-2xl  mx-auto break-words ">
-            <h1 className="text-[40px] text-start ml-3.5  text-orange-500 ">Olá! Meu nome é Kaiky</h1>
+            <h1 className="text-[40px] text-start ml-3.5  text-orange-500 ">
+              Olá! Meu nome é Kaiky
+            </h1>
             <p className=" text-[25px] w-[60vh] font-light m-5 text-justify text-gray-100 leading-relaxed">
               Sou desenvolvedor front-end, e este portfólio mostra um pouco do
               que eu posso construir com código e criatividade.Sou estudante de
-              Ciência da Computação, tenho 19 anos e sou apaixonado por
-              carreira com foco em criar interfaces modernas e responsivas,
-              tecnologia.Estou construindo minha carreira
-              sempre buscando aplicar boas práticas de usabilidade e
-              performance.
+              Ciência da Computação, tenho 19 anos e sou apaixonado por carreira
+              com foco em criar interfaces modernas e responsivas,
+              tecnologia.Estou construindo minha carreira sempre buscando
+              aplicar boas práticas de usabilidade e performance.
             </p>
             <div className="ml-3.5 space-x-8 ">
-            <a
-              href="https://github.com/KaikySsouza"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faGithub} size="3x" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/kaiky-souza-7202a5265/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faLinkedin} size="3x" />
-            </a>
+              <a
+                href="https://github.com/KaikySsouza"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithub} size="3x" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/kaiky-souza-7202a5265/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faLinkedin} size="3x" />
+              </a>
             </div>
-           
           </div>
           <div className="ml-[190px]">
-            <img width={510} src="/images/solo.img2.jpg" alt="solo.png" className="rounded-2xl mt-6" />
+            <img
+              width={510}
+              src="/images/solo.img2.jpg"
+              alt="solo.png"
+              className="rounded-2xl mt-6"
+            />
           </div>
         </div>
       </section>
+
     </div>
   );
 }
