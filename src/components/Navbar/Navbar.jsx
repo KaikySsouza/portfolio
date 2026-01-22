7c283b1074fe583436d9e5da385116ec31614b43
Kaiky Souza <148717444+KaikySsouza@users.noreply.github.com>
2026-01-22 17:41:11 -0300

    fix: corrigido responsividade da tela home.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,9 +7,9 @@ function Navbar() {
     <div>
     
       <div>
-      <nav className="flex justify-between items-center">
-        <li className="m-5 text-[15px]">  &lt;/Kaiky&gt;</li>
-        <ul className="inline-flex space-x-6 m-[10px] text-[15px] text-[#00000]">
+      <nav className="flex justify-between items-center bg-[#011b4b4f]">
+        <li className="m-5 text-[20px]">  &lt;/Kaiky&gt;</li>
+        <ul className="inline-flex space-x-8 m-[10px] text-[15px] text-[#00000]">
           <li className=" hover:bg-[#c8d4e4] border-2 p-2 rounded-2xl cursor-pointer"><Link to={"/"}>Home</Link></li>
           <li className=" hover:bg-[#c8d4e4] border-2 p-2 rounded-2xl cursor-pointer"><Link to={"/about"}>About</Link></li>
           <li className="hover:bg-[#c8d4e4] border-2 p-2 rounded-2xl cursor-pointer"><Link to={"/project"}>Project</Link></li>
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,113 +1,139 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { Cloud, renderSimpleIcon } from "react-icon-cloud";
-import { siJavascript, siNextdotjs, siReact, siBootstrap, siHtml5, siCss, siNodedotjs, siTailwindcss, siExpress, siFigma,
- siGithub, siGit, siSequelize, siMysql, siMongodb, siFirebase, siClickup, 
- siTypescript} from "simple-icons";
+import {
+  siJavascript,
+  siNextdotjs,
+  siReact,
+  siBootstrap,
+  siHtml5,
+  siCss,
+  siNodedotjs,
+  siTailwindcss,
+  siExpress,
+  siFigma,
+  siGithub,
+  siGit,
+  siSequelize,
+  siMysql,
+  siMongodb,
+  siFirebase,
+  siClickup,
+  siTypescript,
+} from "simple-icons";
 
 function Home() {
-  
-  const maquina = document.getElementById('maquina')
+  const maquina = document.getElementById("maquina");
 
   // eslint-disable-next-line no-undef
   const typewriter = new Typewriter(maquina, {
-   autoStart: true,
-   loop: true,
-   delay: 75,
-});
+    autoStart: false,
+    loop: true,
+    delay: 75,
+  });
 
+  // eslint-disable-next-line no-undef
+  typewriter
 
-// eslint-disable-next-line no-undef
-typewriter
-   
-  .typeString('Meu nome é </Kaiky>')
-  .pauseFor(2500)
-  .deleteChars(20)
-  .typeString('Seja bem vindo ao meu portfolio')
-  .pauseFor(2500)
-  .start()
+    .typeString("Meu nome é </Kaiky>")
+    .pauseFor(2500)
+    .deleteChars(20)
+    .typeString("Seja bem vindo ao meu portfolio")
+    .pauseFor(2500)
+    .start();
 
-
-  const icons = [siJavascript,siTypescript, siNextdotjs, siReact, siBootstrap, siHtml5, siCss, siNodedotjs, siTailwindcss,
-    siExpress, siFigma, siGithub, siGit, siSequelize, siMysql, siMongodb, siFirebase, siClickup
+  const icons = [
+    siJavascript,
+    siTypescript,
+    siNextdotjs,
+    siReact,
+    siBootstrap,
+    siHtml5,
+    siCss,
+    siNodedotjs,
+    siTailwindcss,
+    siExpress,
+    siFigma,
+    siGithub,
+    siGit,
+    siSequelize,
+    siMysql,
+    siMongodb,
+    siFirebase,
+    siClickup,
   ].map((icon) => {
-  return renderSimpleIcon({
-    icon,
-    size: 62,
-    aProps: {
-      onClick: (e) => e.preventDefault()
-    }
-  })
-})
-
-
-
-
-
+    return renderSimpleIcon({
+      icon,
+      size: 62,
+      aProps: {
+        onClick: (e) => e.preventDefault(),
+      },
+    });
+  });
 
   return (
-    <div>
-
-      <section className="flex text-left items-center justify-center min-h-screen ">
-        <div className="flex">
-          <div className="">
-            <h1 id="maquina" className="text-[35px] mb-4 m-5"> </h1>
-            <p className="text-[25px] text-justify w-[120vh] justify-between m-2 ">
+    <div className="">
+      <section className="flex items-center min-h-screen ">
+        <div className="flex gap-[15vh] ">
+          <div className="m-4 ">
+            <h1 id="maquina" className="text-[35px] mt-[5vh] m-3 flex-col">
+              {" "}
+            </h1>
+            <p className="text-[20px] w-[95vh] text-justify  indent-3">
               Sou desenvolvedor front-end, e este portfólio mostra um pouco do
               que eu posso construir com código e criatividade.Sou estudante de
-              Ciência da Computação, tenho 19 anos e sou apaixonado por carreira
+              Ciência da Computação,tenho 19 anos e sou apaixonado por carreira
               com foco em criar interfaces modernas e responsivas,
               tecnologia.Estou construindo minha carreira sempre buscando
               aplicar boas práticas de usabilidade e performance.
             </p>
-
-            <div className=""> 
-            <div className="bg-[#1a1b268a] hover:bg-[#19212c]  m-2 w-[37vh] text-center p-[10px] rounded-2xl">
-              <a href="youtube.com.br">Currículo</a>
-            </div>
-
-            <div className="inline-flex  space-x-8 mt-[5vh] m-2">
-              <div className="bg-[#1a1b268a] w-[95px] h-[52px] rounded-2xl hover:bg-[#19212c]">
-              <a
-                href="https://github.com/KaikySsouza"
-                target="_blank"
-                rel="noopener noreferrer" className="p-6"
-              >
-                <FontAwesomeIcon icon={faGithub} size="3x" className="mt-1" />
-              </a>
-              </div>
-              <div className="bg-[#1a1b268a] w-[95px] h-[52px] rounded-2xl hover:bg-[#19212c]">
-              <a
-                href="https://www.linkedin.com/in/kaiky-souza-7202a5265/"
-                target="_blank"
-                rel="noopener noreferrer" className="p-7"
-              >
-                <FontAwesomeIcon icon={faLinkedin} size="3x" className="mt-1"/>
-              </a>
-              </div>
-              </div>
-            </div>
           </div>
-          <div className="">
-             <Cloud>
-    {icons}
-    <a
-      href="https://emojipedia.org/globe-showing-americas/"
-      target="_blank"
-      rel="noopener"
-    >
-      <img
-        height="42"
-        width="42"
-        alt="A globe"
-        src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/globe-showing-americas_1f30e.png"
-      />
-    </a>
-  </Cloud>
-          </div> 
+          <div className="flex relative "> 
+            <Cloud>
+              {icons}
+              <a
+                href="https://emojipedia.org/globe-showing-americas/"
+                target="_blank"
+                rel="noopener"
+              >
+                <img
+                  height="42"
+                  width="42"
+                  alt="A globe"
+                  src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/globe-showing-americas_1f30e.png"
+                />
+              </a>
+            </Cloud>
+          </div>
         </div>
-      </section>
+        {/*      <div className="bg-[#1a1b268a] hover:bg-[#19212c]  m-2 w-[37vh] text-center p-[10px] rounded-2xl">
+          <a href="youtube.com.br">Currículo</a>
+        </div>
 
+    <div className="inline-flex  space-x-8 mt-[5vh] m-2">
+          <div className="bg-[#1a1b268a] w-[95px] h-[52px] rounded-2xl hover:bg-[#19212c]">
+            <a
+              href="https://github.com/KaikySsouza"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-6"
+            >
+              <FontAwesomeIcon icon={faGithub} size="3x" className="mt-1" />
+            </a>
+          </div>
+          <div className="bg-[#1a1b268a] w-[95px] h-[52px] rounded-2xl hover:bg-[#19212c]">
+            <a
+              href="https://www.linkedin.com/in/kaiky-souza-7202a5265/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-7"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="3x" className="mt-1" />
+            </a>
+          </div>
+        </div>
+        */}
+      </section>
     </div>
   );
 }
