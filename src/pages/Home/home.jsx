8b9d99d4456280eacb55901892f7cffc7af65f12
Kaiky Souza <kaikybelga@gmail.com>
2026-08-21 13:43:28 -0300

    feat: Adicionado links aos botões e icones de navegação

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -43,15 +43,17 @@ function Home() {
 
         <div className=" flex justify-baseline space-x-10 text-center items-center ">
         <button className="bg-[#22C55E] p-4 w-[18vh] rounded-2xl cursor-pointer text-white"> Ver projetos</button>
-        <button className="rounded-2xl p-4  border-white border-1 cursor-pointer text-white flex justify-center w-[18vh]  ">Baixar CV <GoDownload  className="m-1"/>
-        </button>
+         <button className="rounded-2xl p-4  border-white border-1 cursor-pointer text-white  flex justify-center w-[18vh]  ">   <a className="flex" href="public/pdf/Kaiky_Souza_Curriculo.pdf"  target="_blank" rel="noopener noreferrer" >Baixar CV <GoDownload  className="m-1"/></a>  </button> 
+      
         </div>
      
       <div className="flex justify-baseline gap-8 text-[26px] absolute bottom-20  " >
-        <FaGithub className="text-white"/>
-        <TbBrandLinkedin  className="text-white"/>
-        <FaInstagram className="text-white"/>
-        <MdOutlineEmail className="text-white"/>
+       <a href="https://github.com/KaikySsouza" target="_blank" rel="noopener noreferrer"><FaGithub className="text-white"/></a> 
+        <a href="https://www.linkedin.com/in/kaiky-souza-7202a5265/" target="_blank" rel="noopener noreferrer"><TbBrandLinkedin  className="text-white"/></a> 
+        <a href="https://www.instagram.com/kaiky_sl/" target="_blank" rel="noopener noreferrer"><FaInstagram className="text-white"/></a>
+        <a href="mailto:kaikybelga@gmail.com?subject=Contato%20pelo%20portfólio&body=Olá,%20vi%20seu%20portfólio%20e%20gostaria%20de%20entrar%20em%20contato." aria-label="Enviar e-mail">
+        <i class="fa-solid fa-envelope"><MdOutlineEmail className="text-white"/></i>
+        </a>
         
        </div>
  
